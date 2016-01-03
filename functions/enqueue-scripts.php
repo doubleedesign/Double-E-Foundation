f820f69bd61dc38d26f4ec1d74a5300ea3169f19
Leesa Ward <leesa@doubleedesign.com.au>
2016-01-03 18:40:55 +1100

    1.0.1 Fixing JavaScript

--- functions/enqueue-scripts.php
+++ functions/enqueue-scripts.php
@@ -1,16 +1,16 @@
 <?php
 /**
- * Enqueue all styles and scripts
+ * Enqueue all scripts
  *
  * Learn more about enqueue_script: {@link https://codex.wordpress.org/Function_Reference/wp_enqueue_script}
  *
  * @package WordPress
- * @subpackage FoundationPress
- * @since FoundationPress 1.0.0
+ * @subpackage Double-E-Foundation
+ * @since Double-E-Foundation 1.1.1
  */
 
-if ( ! function_exists( 'foundationpress_scripts' ) ) :
-	function foundationpress_scripts() {
+if ( ! function_exists( 'doublee_scripts' ) ) :
+	function doublee_scripts() {
 
 	// Deregister the jquery version bundled with WordPress.
 	wp_deregister_script( 'jquery' );
@@ -18,9 +18,12 @@ if ( ! function_exists( 'foundationpress_scripts' ) ) :
 	// CDN hosted jQuery placed in the header, as some plugins require that jQuery is loaded in the header.
 	wp_enqueue_script( 'jquery', '//ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js', array(), '2.1.0', false );
 
-	// If you'd like to cherry-pick the foundation components you need in your project, head over to gulpfile.js and see lines 35-54.
-	// It's a good idea to do this, performance-wise. No need to load everything if you're just going to use the grid anyway, you know :)
-	wp_enqueue_script( 'foundation', get_template_directory_uri() . '/assets/javascript/foundation.js', array('jquery'), '2.2.0', true );
+	// Load desired Foundation JS components. More can be found in Double-E-Foundation/assets/javascript.
+	$theme = wp_get_theme();
+	$version = $theme->get( 'Version' );
+	wp_enqueue_script( 'foundation.core', get_template_directory_uri() . '/assets/javascript/foundation.core.js', array('jquery'), $version, true );
+	wp_enqueue_script( 'foundation.dropdown', get_template_directory_uri() . '/assets/javascript/foundation.dropdown.js', array('jquery'), $version, true );
+	wp_enqueue_script( 'foundation.dropdownMenu', get_template_directory_uri() . '/assets/javascript/foundation.dropdownMenu.js', array('jquery'), $version, true );
 
 	// Add the comment-reply library on pages where it is necessary
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
@@ -29,7 +32,7 @@ if ( ! function_exists( 'foundationpress_scripts' ) ) :
 
 	}
 
-	add_action( 'wp_enqueue_scripts', 'foundationpress_scripts' );
+	add_action( 'wp_enqueue_scripts', 'doublee_scripts' );
 endif;
 
 ?>
--- functions/enqueue-styles.php
+++ functions/enqueue-styles.php
@@ -1,26 +1,37 @@
 <?php
 /**
- * Enqueue all styles scripts
+ * Enqueue all scripts
  *
  * Learn more about enqueue_script: {@link https://codex.wordpress.org/Function_Reference/wp_enqueue_script}
  *
  * @package WordPress
  * @subpackage Double-E-Foundation
- * @since Double-E-Foundation 0.0.2
+ * @since Double-E Foundation 1.1.1
  */
 
-if ( ! function_exists( 'doublee_styles' ) ) :
+if ( ! function_exists( 'doublee_scripts' ) ) :
+	function doublee_scripts() {
 
-	function doublee_styles() {
+	// Deregister the jquery version bundled with WordPress.
+	wp_deregister_script( 'jquery' );
 
-		// Enqueue the main Stylesheet.
-		$theme = wp_get_theme();
-		$version = $theme->get( 'Version' );
-		wp_enqueue_style( 'main-stylesheet', get_stylesheet_directory_uri() . '/style.css', array(), $version, 'all' );
+	// CDN hosted jQuery placed in the header, as some plugins require that jQuery is loaded in the header.
+	wp_enqueue_script( 'jquery', '//ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js', array(), '2.1.0', false );
+
+	// Load desired Foundation JS components. More can be found in Double-E-Foundation/assets/javascript.
+	$theme = wp_get_theme();
+	$version = $theme->get( 'Version' );
+	wp_enqueue_script( 'foundation.dropdown', get_template_directory_uri() . '/assets/javascript/foundation.dropdown.js', array('jquery'), $version, true );
+	wp_enqueue_script( 'foundation.dropdownMenu', get_template_directory_uri() . '/assets/javascript/foundation.dropdownMenu.js', array('jquery'), $version, true );
+
+	// Add the comment-reply library on pages where it is necessary
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
 
 	}
-	
-	add_action( 'wp_enqueue_scripts', 'doublee_styles' );
+
+	add_action( 'wp_enqueue_scripts', 'doublee_scripts' );
 endif;
 
 ?>
