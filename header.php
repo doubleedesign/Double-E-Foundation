c91cfa62e8a35ce32dc36ed612bdf8eb76daf194
Leesa Ward <leesa@doubleedesign.com.au>
2016-12-11 22:12:54 +1100

    Tweaked CSS for offcanvas, added FontAwesome

--- header.php
+++ header.php
@@ -36,7 +36,7 @@
 	<header id="masthead" class="site-header" role="banner">
 	
 		<div class="title-bar" data-responsive-toggle="site-navigation">
-			<button class="menu-icon" type="button" data-toggle="offCanvas"></button>
+			<button class="menu-icon" type="button" data-toggle="offCanvas"><i class="fa fa-bars"></i></button>
 			<div class="title-bar-title">
 				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
 			</div>
