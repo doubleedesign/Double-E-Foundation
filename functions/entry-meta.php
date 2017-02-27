5f83726c902e633c8db2245c0600032bd251a4f5
Leesa Ward <leesa@doubleedesign.com.au>
2017-02-28 09:46:41 +1100

    Fixed the entry meta function

--- functions/entry-meta.php
+++ functions/entry-meta.php
@@ -4,13 +4,12 @@
  *
  * @package WordPress
  * @subpackage Double-E Foundation
- * @since Double-E Foundation 0.1.4
+ * @since Double-E Foundation 2.1
  */
 
 if ( ! function_exists( 'doublee_entry_meta' ) ) :
 	function doublee_entry_meta() {
-		echo '<time class="date">Posted on ' . the_date('l, F j, Y') . '</time>';
-		echo '<p class="byline author">'. __( 'Posted by', '' ) .' <a href="'. get_author_posts_url( get_the_author_meta( 'ID' ) ) .'" rel="author">'. get_the_author() .'</a></p>';
+		echo '<p class="byline author">'. __( 'Posted by', '' ) .' <a href="'. get_author_posts_url( get_the_author_meta( 'ID' ) ) .'" rel="author">'. get_the_author() .'</a> on <time class="date">' . get_the_date('l, F j, Y') . '</time></p>';
 	}
 endif;
 ?>
