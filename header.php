112bb01046e7f02ddf96983658a3fcd592d56c9d
Leesa Ward <leesa@doubleedesign.com.au>
2017-04-02 16:46:20 +1000

    Correct parts/ to template-parts/

--- header.php
+++ header.php
@@ -41,7 +41,7 @@ $parent_class = 'parent-'.$parent->post_name;
 	<?php if ( get_theme_mod( 'wpt_mobile_menu_layout' ) == 'offcanvas' ) { ?>
 	<div class="off-canvas-wrapper fadeIn animated">
 		<div class="off-canvas-wrapper-inner" data-off-canvas-wrapper>
-		<?php get_template_part( 'parts/mobile-off-canvas' ); ?>
+		<?php get_template_part( 'template-parts/mobile-off-canvas' ); ?>
 	<?php } ?>
 
 	<header id="masthead" class="site-header" role="banner">
