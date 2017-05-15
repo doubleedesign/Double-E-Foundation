e5bc5bac628c16427ad86ef167eed3ca1db9dca9
Leesa Ward <leesa@doubleedesign.com.au>
2017-05-16 09:44:54 +1000

    Removed call to widget area in sidebar.php as it's not registered by default

--- sidebar.php
+++ sidebar.php
@@ -4,10 +4,10 @@
  *
  * @package WordPress
  * @subpackage Double-E Foundation
- * @since Double-E Foundation 0.1.4
+ * @since Double-E Foundation 2.2.3
  */
 
 ?>
 <aside id="sidebar" class="small-12 medium-4 columns">
-	<?php dynamic_sidebar( 'sidebar-widgets' ); ?>
+	
 </aside>
