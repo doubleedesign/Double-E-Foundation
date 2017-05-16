982dc63c61e1728bd47c581864706612bf629ff7
Leesa Ward <leesa@doubleedesign.com.au>
2017-05-16 10:51:29 +1000

    Fixed prefix

--- templates/page-styleguide.php
+++ templates/page-styleguide.php
@@ -18,7 +18,7 @@
            <header>
                <h1 class="entry-title">
                     <?php
-				   		$visual_page_title = get_post_meta( $post->ID, 'stm_visual-page-title-entry', true ); 
+				   		$visual_page_title = get_post_meta( $post->ID, 'doublee_visual-page-title-entry', true ); 
 				   		if (!empty($visual_page_title)) {
 							echo $visual_page_title. ' Heading 1';
 						} else {
