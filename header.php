07dbe863f8b3dabd9b515499eeb6294c7a4713e8
Leesa Ward <leesa@doubleedesign.com.au>
2017-06-05 13:57:18 +1000

    Added browser warning for IE9 and below

--- header.php
+++ header.php
@@ -45,6 +45,8 @@ $parent_class = 'parent-'.$parent->post_name;
 		<div class="off-canvas-wrapper-inner" data-off-canvas-wrapper>
 		<?php get_template_part( 'template-parts/mobile-off-canvas' ); ?>
 	<?php } ?>	
+	
+	<!--[if lte IE 9]><div class="alert callout"><span>You are using an outdated version of Microsoft's Internet Explorer web browser, which does not display modern web sites properly (including this one). To ensure you always see sites at their best and your browser security is up to date, it is highly recommended that you <a href="http://browsehappy.com/" target="_blank">upgrade to a modern browser</a>.</span></div><![endif]-->
 
 	<header id="masthead" class="site-header" role="banner">
 	
