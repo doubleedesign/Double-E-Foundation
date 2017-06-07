1ae9771dff29aa91f492f3dd8edc3f4f86e8f8db
Leesa Ward <leesa@doubleedesign.com.au>
2017-06-07 15:19:12 +1000

    Removed set_post_thumbail size

--- functions/media.php
+++ functions/media.php
@@ -14,7 +14,6 @@
 // Set post thumbnail size and add additional image sizes
 if ( ! function_exists( 'doublee_images' ) ) :
 	function doublee_images() {
-		set_post_thumbnail_size(480,480,true);	
 		add_image_size('banner',1920,580,true);
 	}
 	add_action( 'after_setup_theme', 'doublee_images' );
