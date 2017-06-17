06ca751dbf738a6bf90053cfb93dd0745b8b5d48
Leesa Ward <leesa@doubleedesign.com.au>
2017-06-18 02:20:45 +1000

    Set some sensible defaults for the top bar

--- template-parts/top-bar.php
+++ template-parts/top-bar.php
@@ -1,18 +1,16 @@
 <div id="top-bar" class="main-navigation top-bar" role="navigation">
-	<div class="row">
-		<div class="small-12 columns">
-			<div class="top-bar-left">
-				<ul class="menu">
-					<li class="home"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></li>
-				</ul>
+	<div class="row align-center align-justify">
+		<div class="small-12 large-4 columns">
+			<div id="logo" class="row align-middle">
+				<div class="columns"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></div>
 			</div>
-			<nav id="site-navigation" class="top-bar-right" data-toggler>
-				<?php doublee_top_bar_menu(); ?>
-
-				<?php if ( ! get_theme_mod( 'wpt_mobile_menu_layout' ) || get_theme_mod( 'wpt_mobile_menu_layout' ) == 'topbar' ) : ?>
-					<?php get_template_part( 'template-parts/mobile-top-bar' ); ?>
-				<?php endif; ?>
-			</nav>
 		</div>
+		<nav id="site-navigation" class="small-12 large-8 columns" data-toggler>
+			<?php doublee_top_bar_menu(); ?>
+
+			<?php if ( ! get_theme_mod( 'wpt_mobile_menu_layout' ) || get_theme_mod( 'wpt_mobile_menu_layout' ) == 'topbar' ) : ?>
+				<?php get_template_part( 'template-parts/mobile-top-bar' ); ?>
+			<?php endif; ?>
+		</nav>
 	</div>
 </div>
