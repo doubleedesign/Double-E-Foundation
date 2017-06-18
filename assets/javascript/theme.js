196718bcec489a0b98f5593fa64b554784bafd66
Leesa Ward <leesa@doubleedesign.com.au>
2017-06-18 18:48:03 +1000

    Use jQuery instead of $ to minimise issues

--- assets/javascript/theme.js
+++ assets/javascript/theme.js
@@ -1,31 +1,31 @@
 //@prepros-prepend foundation.js
 //@prepros-prepend vendor/what-input.min.js
 
-$(document).ready(function() {
+jQuery(document).ready(function() {
 	
 	"use strict";
 	
 	// Initialise Foundation
-    jQuery(document).foundation();
+      jQuery(document).foundation();
 	
 	// Add Flex Video classes to YouTube and Vimeo. Thanks FoundationPress
 	jQuery( 'iframe[src*="youtube.com"]').wrap("<div class='flex-video widescreen'/>");
 	jQuery( 'iframe[src*="vimeo.com"]').wrap("<div class='flex-video widescreen vimeo'/>");
 	
 	// Slide the mobile topbar menu
-	$('.title-bar .menu-icon').click(function() {
-		$('#site-navigation').slideToggle(400);
+	jQuery('.title-bar .menu-icon').click(function() {
+		jQuery('#site-navigation').slideToggle(400);
 	});
 		
 });
 
 // Sticky Footer - thanks FoundationPress
-$(window).bind(' load resize orientationChange ', function () {
+jQuery(window).bind(' load resize orientationChange ', function () {
  	"use strict";
 	
-	var footer = $("#footer-container");
+	var footer = jQuery("#footer-container");
 	var pos = footer.position();
-	var height = $(window).height();
+	var height = jQuery(window).height();
 	height = height - pos.top;
 	height = height - footer.height() -1;
 
