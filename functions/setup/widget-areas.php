19fcd173140c509aa557f963a6a6f6e37af8f0e0
Leesa Ward <leesa@doubleedesign.com.au>
2017-06-18 14:17:05 +1000

    Added a widget area for the shop page

--- functions/setup/widget-areas.php
+++ functions/setup/widget-areas.php
@@ -1,2 +1,20 @@
 <?php
-// No widgets for this site.
+
+if (class_exists('Woocommerce')) {
+	
+	function doublee_woo_sidebar() {
+		register_sidebar( array(
+		'name' 		=> __( 'Shop sidebar', 'double-e-foundation' ),
+		'id' 			=> 'shop-sidebar',
+		'description' 	=> __( 'Widgets in this area will be shown in the sidebar on the shop page', 'double-e-foundation' ),
+		'before_widget' 	=> '<div id="%1$s" class="widget %2$s">',
+		'after_widget'  	=> '</div>',
+		'before_title'  	=> '<h3 class="widget-title">',
+		'after_title'   	=> '</h3>',
+		) );
+	}
+	
+	add_action( 'widgets_init', 'doublee_woo_sidebar' );
+}
+
+?>
