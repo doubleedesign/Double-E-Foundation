d17783de1fdd84071782de2c0321f194fd47f7bf
Leesa Ward <leesa@doubleedesign.com.au>
2017-06-23 13:22:02 +1000

    Make search form layout more dynamic

--- searchform.php
+++ searchform.php
@@ -4,18 +4,18 @@
  *
  * @package WordPress
  * @subpackage Double-E Foundation
- * @since FoundationPress 1.0.0
+ * @since Double-E Foundation 2.3.0
  */
 ?>
 <form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
 	<div class="row collapse">
 
-		<div class="small-8 columns">
-			<input type="text" value="" name="s" id="s" placeholder="<?php esc_attr_e( 'Search', '' ); ?>">
+		<div class="expand columns">
+			<input type="text" value="" name="s" id="s" placeholder="<?php esc_attr_e( 'Search our site', '' ); ?>">
 		</div>
 
-		<div class="small-4 columns">
-			<input type="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', '' ); ?>" class="prefix button">
+		<div class="shrink columns">
+			<input type="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', '' ); ?>" class="success button">
 		</div>
 
 	</div>
