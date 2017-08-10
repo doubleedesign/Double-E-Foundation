17dead378d29ee659d46e41b6e1bbfe86d85f161
Leesa Ward <leesa@doubleedesign.com.au>
2017-08-10 10:48:37 +1000

    Removed unused argument

--- functions/navigation/menu-classes.php
+++ functions/navigation/menu-classes.php
@@ -4,13 +4,13 @@
  *
  * @package WordPress
  * @subpackage Double-E-Foundation
- * @since Double-E-Foundation 2.2.3
+ * @since Double-E-Foundation 2.4.0
  */
 
 /* ==========================================
 	ADD SLUGS TO NAVIGATION MENU ITEMS
 ============================================*/
-function doublee_menu_classes( $atts, $item, $args ) {
+function doublee_menu_classes( $atts, $item ) {
     // Get the menu item title and put it into lowercase
 	$slug = strtolower($item->title);
 	// Remove ampersands
@@ -22,6 +22,6 @@ function doublee_menu_classes( $atts, $item, $args ) {
 	$atts['class'] = 'menu-item-'.$slug;
     return $atts;
 }
-add_filter( 'nav_menu_css_class', 'doublee_menu_classes', 10, 3 );
+add_filter( 'nav_menu_css_class', 'doublee_menu_classes', 10, 2 );
 
 ?>
