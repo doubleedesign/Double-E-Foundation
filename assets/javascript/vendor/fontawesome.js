04a28b4e6687509fc00c7aa9c0e729b002d2b42d
Leesa Ward <leesa@doubleedesign.com.au>
2018-04-10 23:54:25 +1000

    Upgraded Font Awesome to version 5.0.9

---
+++ assets/javascript/vendor/fontawesome.js
@@ -0,0 +1,7 @@
+// Font Awesome
+// Note: In the individual font files, it is highly recommended to comment out unused icons to reduce file size.
+FontAwesomeConfig = { searchPseudoElements: true };
+//@prepros-append ../../fonts/fontawesome.js
+//@prepros-append ../../fonts/fa-regular.js
+//@prepros-append ../../fonts/fa-solid.js
+//@prepros-append ../../fonts/fa-brands.js
