3c5ce7b65e1d017cd9b098d4433cf4a5127f5b88
Leesa Ward <leesa@doubleedesign.com.au>
2018-04-10 22:57:12 +1000

    Improved documentation

--- functions/misc.php
+++ functions/misc.php
@@ -4,40 +4,57 @@
  *
  * @package WordPress
  * @subpackage Double-E-Foundation
- * @since Double-E-Foundation 2.3.0
+ * @since Double-E-Foundation 3.0
  */
 
-/* ==========================================
-	ADD PARENT PAGE SLUG TO THE BODY CLASS
-	(Adds both to the parent page itself and its child pages)
-============================================*/
-function doublee_body_class_section($classes) {  
-	global $wpdb, $post;  
+
+/**
+ * Add classes to the body tag
+ * @param $classes
+ * @return array
+ * @since Double-E Foundation 3.0
+ */
+function doublee_body_classes($classes) {
+	// Get some info, set some variables for later use
+	global $post;
 	$current_page_id = $post->ID;
 	$ancestors = get_post_ancestors($current_page_id);
-	if (is_page()) {  
-		if ($post->post_parent) {  
-			$parent  = end($ancestors);  
-		} else {  
-			$parent = $post->ID;  
-		}  
-		$post_data = get_post($parent, ARRAY_A);  
-		$classes[] = 'section-' . $post_data['post_name'];  
-	}  
-	return $classes;  
-}  
-add_filter('body_class', 'doublee_body_class_section'); 
+
+	// Page parent/section
+	if (is_page()) {
+		if ($post->post_parent) {
+			$parent  = end($ancestors);
+		} else {
+			$parent = $post->ID;
+		}
+		$post_data = get_post($parent, ARRAY_A);
+		$classes[] .= 'section-' . $post_data['post_name'];
+	}
+
+	// Browser
+	$browser = apso_get_browser();
+	$classes[] .= $browser;
+
+	// Shall we animate?
+	if($browser != 'ie') {
+		$classes[] .= 'layout-animations-supported';
+	}
+
+	// Return the classes for use by the filter
+	return $classes;
+}
+add_filter('body_class', 'doublee_body_classes');
 
 
-/* ==========================================
-	ALLOW SHORTCODES IN CATEGORY/TERM DESCRIPTIONS
-============================================*/
+/**
+ * Allow shortcodes in category/term descriptions
+ */
 add_filter( 'term_description', 'do_shortcode');
 
 
-/* ==========================================
-	REMOVE EXTRANEOUS P TAGS FROM SHORTCODES
-============================================*/
+/**
+ * Remove extraneous p tags from shortcodes
+ */
 // Remove wpautop from wherever it is
 remove_filter('the_content', 'wpautop');
 remove_filter('term_description', 'wpautop');
@@ -49,11 +66,11 @@ add_filter('the_content', 'shortcode_unautop', 100);
 add_filter('term_description', 'shortcode_unautop', 100);
 
 
-/* ==========================================
-	CHANGE UPLOAD DIRECTORY FOR PDF FILES
-	Source: https://wordpress.stackexchange.com/questions/47415/change-upload-directory-for-pdf-files
-	** This should eventually be put into a plugin **
-============================================*/
+/**
+ * Change upload directory for PDF files
+ * Source: https://wordpress.stackexchange.com/questions/47415/change-upload-directory-for-pdf-files
+ * TODO: This should probably be in a plugin
+ */
 
 add_filter('wp_handle_upload_prefilter', 'doublee_pre_upload');
 add_filter('wp_handle_upload', 'doublee_post_upload');
@@ -80,11 +97,11 @@ function doublee_custom_upload_dir($path){
     return $path;
 }
 
-/* ==========================================
-	MOVE YOAST SEO TO THE BOTTOM OF EDIT SCREENS
-============================================*/
+
+/**
+ * Move Yoast SEO to the bottom of edit screens
+ */
 add_filter( 'wpseo_metabox_prio', 'doublee_move_yoast_seo_metabox' );
 function doublee_move_yoast_seo_metabox() {
 	return 'low';
 }
-?>
