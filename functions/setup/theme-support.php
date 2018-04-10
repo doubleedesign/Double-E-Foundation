50f20e7a3d6c2e8482ce33b72dad5d15bb172c40
Leesa Ward <leesa@doubleedesign.com.au>
2018-04-10 23:19:21 +1000

    Improved formatting and simplified theme support

--- functions/setup/theme-support.php
+++ functions/setup/theme-support.php
@@ -4,50 +4,49 @@
  *
  * @package WordPress
  * @subpackage Double-E Foundation
- * @since Double-E Foundation 2.0
+ * @since Double-E Foundation 3.0.0
+ *
+ * Note: WooCommerce support is declared in functions/woocommerce.php due to the custom settings available
  */
 
-if ( ! function_exists( 'doublee_theme_support' ) ) :
+if ( ! function_exists( 'doublee_theme_support' ) ) {
 	function doublee_theme_support() {
 		// Add language support
-		load_theme_textdomain( '', get_template_directory() . '/languages' );
+		load_theme_textdomain('', get_template_directory() . '/languages');
 
 		// Add menu support
-		add_theme_support( 'menus' );
+		add_theme_support('menus');
 
 		// Let WordPress manage the document title
-		add_theme_support( 'title-tag' );
+		add_theme_support('title-tag');
 
 		// Add post thumbnail support: http://codex.wordpress.org/Post_Thumbnails
-		add_theme_support( 'post-thumbnails' );
+		add_theme_support('post-thumbnails');
 
 		// RSS 
-		add_theme_support( 'automatic-feed-links' );
+		add_theme_support('automatic-feed-links');
 
 		// Add post formats support: http://codex.wordpress.org/Post_Formats
-		add_theme_support( 'post-formats', array('aside', 'gallery', 'link', 'image', 'quote', 'status', 'video', 'audio', 'chat') );
+		add_theme_support('post-formats', array('gallery', 'video'));
 
 		// Use HTML5 for some things that don't use it by default
-		add_theme_support( 'html5', array(
+		add_theme_support('html5', array(
 			'gallery',
 			'search-form',
 			'caption'
 		));
-
-		// Declare WooCommerce support per http://docs.woothemes.com/document/third-party-custom-theme-compatibility/
-		add_theme_support( 'woocommerce' );
 	}
 
-	add_action( 'after_setup_theme', 'doublee_theme_support' );
-endif;
+	add_action('after_setup_theme', 'doublee_theme_support');
 
-if ( ! function_exists( 'doublee_post_type_support' ) ) :
+}
+
+if ( ! function_exists( 'doublee_post_type_support' ) ) {
 	function doublee_post_type_support() {
 
 		// Add Excerpt support to pages
-		add_post_type_support( 'page', 'excerpt' );
+		add_post_type_support('page', 'excerpt');
 	}
 
-	add_action( 'init', 'doublee_post_type_support' );
-endif;
-?>
+	add_action('init', 'doublee_post_type_support');
+}
--- functions/woocommerce.php
+++ functions/woocommerce.php
@@ -4,14 +4,24 @@
  *
  * @package WordPress
  * @subpackage Double-E-Foundation
- * @since Double-E-Foundation 2.2.3
+ * @since Double-E-Foundation 3.0.0
  */
 
-// Declare theme support for WooCommerce
-add_theme_support( 'woocommerce' ); 
+// Declare WooCommerce support with custom settings
+function doublee_woocommerce_support() {
+	add_theme_support( 'woocommerce', array(
+		// Declaring these means changes in the Customiser have no effect.
+		/*'thumbnail_image_width' => 600,
+		'gallery_thumbnail_image_width' => 600,
+		'single_image_width'    => 600,*/
+	) );
+}
+add_action( 'after_setup_theme', 'doublee_woocommerce_support' );
+
 
 // Don't load WooCommerce's CSS files
 add_filter( 'woocommerce_enqueue_styles', '__return_false' );
+
 
 // Add an option to sort products alphabetically
 function doublee_woocommerce_get_catalog_ordering_args( $args ) {
@@ -34,7 +44,3 @@ add_filter( 'woocommerce_get_catalog_ordering_args', 'doublee_woocommerce_get_ca
 add_filter( 'woocommerce_default_catalog_orderby_options', 'doublee_woocommerce_catalog_orderby' );
 add_filter( 'woocommerce_catalog_orderby', 'doublee_woocommerce_catalog_orderby' );
 // end sort products alphabetically
-
-
-
-?>
