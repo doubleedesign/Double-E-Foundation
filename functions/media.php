7e2f693df9afc60e2992e33815907e8f8e18dcf2
Leesa Ward <leesa@doubleedesign.com.au>
2018-05-21 13:49:11 +1000

    Altered default gallery markup to use Swipebox

--- functions/media.php
+++ functions/media.php
@@ -4,7 +4,7 @@
  *
  * @package WordPress
  * @subpackage Double-E-Foundation
- * @since Double-E-Foundation 3.0.0
+ * @since Double-E-Foundation 3.0.1
  */
 
 /**
@@ -16,6 +16,157 @@ if ( ! function_exists( 'doublee_images' ) ) :
 	}
 	add_action( 'after_setup_theme', 'doublee_images' );
 endif;
+
+
+/**
+ * Alter the default gallery markup to:
+ * - Incorporate Swipebox
+ * - Remove <br> tags
+ * - Use the markup we want (by default, galleries are definition lists unless theme support for HTML5 has been added and galleries included in that;
+ * 	 this allows further customisation than that
+ * Adapted from: http://robido.com/wordpress/wordpress-gallery-filter-to-modify-the-html-output-of-the-default-gallery-shortcode-and-style/
+ * @param $output
+ * @param $attr
+ * @return string
+ */
+function doublee_post_gallery_markup( $output, $attr ) {
+
+	// Initialize
+	global $post, $wp_locale;
+
+	// Gallery instance counter
+	static $instance = 0;
+	$instance++;
+
+	// Validate the author's orderby attribute
+	if ( isset( $attr['orderby'] ) ) {
+		$attr['orderby'] = sanitize_sql_orderby( $attr['orderby'] );
+		if ( ! $attr['orderby'] ) unset( $attr['orderby'] );
+	}
+
+	// Get attributes from shortcode
+	extract(shortcode_atts(array(
+		'order'      => 'ASC',
+		'orderby'    => 'menu_order ID',
+		'id'         => $post->ID,
+		// Change your wrapping elements here if you want/need to
+		'itemtag'    => 'figure',
+		'icontag'    => 'div',
+		'captiontag' => 'figcaption',
+		//
+		'columns'    => 3,
+		'size'       => 'thumbnail',
+		'include'    => '',
+		'exclude'    => ''
+	), $attr ) );
+
+	// Initialize
+	$id = intval($id);
+	$attachments = array();
+	if ($order == 'RAND') $orderby = 'none';
+	if (!empty($include)) {
+		// Include attribute is present
+		$include = preg_replace( '/[^0-9,]+/', '', $include );
+		$_attachments = get_posts(
+			array(
+				'include' => $include,
+				'post_status' => 'inherit',
+				'post_type' => 'attachment',
+				'post_mime_type' => 'image',
+				'order' => $order,
+				'orderby' => $orderby
+			)
+		);
+		// Setup attachments array
+		foreach ($_attachments as $key => $val) {
+			$attachments[ $val->ID ] = $_attachments[ $key ];
+		}
+	} else if (!empty($exclude)) {
+		// Exclude attribute is present
+		$exclude = preg_replace( '/[^0-9,]+/', '', $exclude );
+
+		// Setup attachments array
+		$attachments = get_children(
+			array(
+				'post_parent' => $id,
+				'exclude' => $exclude,
+				'post_status' => 'inherit',
+				'post_type' => 'attachment',
+				'post_mime_type' => 'image',
+				'order' => $order,
+				'orderby' => $orderby
+			)
+		);
+	} else {
+		// Setup attachments array
+		$attachments = get_children(
+			array(
+				'post_parent' => $id,
+				'post_status' => 'inherit',
+				'post_type' => 'attachment',
+				'post_mime_type' => 'image',
+				'order' => $order,
+				'orderby' => $orderby
+			)
+		);
+	}
+	if ( empty( $attachments ) ) return '';
+
+	// Filter gallery differently for feeds
+	if ( is_feed() ) {
+		$output = "\n";
+		foreach ( $attachments as $att_id => $attachment ) $output .= wp_get_attachment_link( $att_id, $size, true ) . "\n";
+		return $output;
+	}
+
+	// Filter tags and attributes
+	$itemtag = tag_escape($itemtag);
+	$captiontag = tag_escape($captiontag);
+	$columns = intval($columns);
+	$itemwidth = $columns > 0 ? floor( 100 / $columns ) : 100;
+	$float = is_rtl() ? 'right' : 'left';
+	$selector = "gallery-{$instance}";
+
+	// Wrapping div; remove this if you don't want your gallery items wrapped
+	$output = apply_filters( 'gallery_style', "<div id='$selector' class='gallery galleryid-{$id} gallery-columns-{$columns} gallery-size-{$size}'>");
+
+	// Iterate through the attachments in this gallery instance
+	foreach ( $attachments as $id => $attachment ) {
+
+		// Build the attachment link
+		if(isset( $attr['link'] ) && 'file' == $attr['link']) { // The gallery is set to link to the image files
+			$id = $attachment->ID;
+			$url = wp_get_attachment_url($id);
+			$title = get_the_title($id);
+			$image_file = wp_get_attachment_image_src($id, $size);
+			$setimage = $image_file[0];
+			$link = '<a class="swipebox" href="'.$url.'" title="'.$title.'"><img src="'.$setimage.'" alt="'.$title.'"/></a>';
+		} else { // It's not (i.e. it's set to attachment page or none)
+			$link = wp_get_attachment_link($id, $size, false, false, false, false);
+		}
+
+		// Start itemtag
+		$output .= "<{$itemtag} class='gallery-item'>";
+		// Add the link
+		$output .= $link;
+		// Add the caption
+		if ( $captiontag && trim( $attachment->post_excerpt ) ) {
+			$output .= "
+		<{$captiontag} class='gallery-caption'>
+			" . wptexturize($attachment->post_excerpt) . "
+		</{$captiontag}>";
+		}
+		// End itemtag
+		$output .= "</{$itemtag}>";
+	}
+
+	// End gallery output
+	$output .= "</div>\n";
+	return $output;
+}
+
+// Apply filter to default gallery shortcode
+add_filter( 'post_gallery', 'doublee_post_gallery_markup', 10, 2 );
 
 
 /**
