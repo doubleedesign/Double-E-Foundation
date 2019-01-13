2fe2ed21aa4876f079ff007a45d7f7892af24d53
Leesa Ward <leesa@doubleedesign.com.au>
2019-01-13 23:42:09 +1100

    Corrected class name for vertical alignment of top bar

--- layout/top-bar.php
+++ layout/top-bar.php
@@ -1,5 +1,5 @@
 <div id="top-bar" class="main-navigation top-bar" role="navigation">
-	<div class="row align-center align-justify">
+	<div class="row align-middle align-justify">
 		<div class="small-12 large-4 columns">
 			<div id="logo" class="row align-middle">
 				<div class="columns"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></div>
