a68a79a9157e48b0504cab748e7a2d3da31cb2c4
Leesa Ward <leesa@doubleedesign.com.au>
2019-01-16 22:17:09 +1100

    Added a template part to get social account links from theme options

---
+++ template-parts/social-links.php
@@ -0,0 +1,13 @@
+<?php if( have_rows('social_media_links', 'option' ) ) { ?>
+	<ul class="social">
+		<?php while( have_rows('social_media_links', 'option') ) {
+			the_row();
+			// Fields
+			$label = get_sub_field('label');
+			$icon = get_sub_field('font_awesome_icon_class');
+			$url = get_sub_field('url');
+			// Output ?>
+			<li><a class="tippy" title="<?php echo $label; ?>" href="<?php echo $url; ?>" target="_blank"><i class="fab <?php echo $icon; ?>"></i></a></li>
+		<?php } ?>
+	</ul>
+<?php } ?>
