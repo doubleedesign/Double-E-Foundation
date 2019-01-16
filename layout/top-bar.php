1c23fcdaafd0d6de96fdccb121a23b45a8798d4a
Leesa Ward <leesa@doubleedesign.com.au>
2019-01-16 22:53:26 +1100

    Tidied up logo/site name code

--- layout/top-bar.php
+++ layout/top-bar.php
@@ -9,8 +9,10 @@
 <div id="top-bar" class="main-navigation top-bar" role="navigation">
 	<div class="row align-middle align-justify">
 		<div class="small-12 large-4 columns">
-			<div id="logo" class="row align-middle">
-				<div class="columns"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></div>
+			<div id="logo">
+				<a href="<?php echo site_url(); ?>" rel="home">
+					<?php bloginfo( 'name' ); ?>
+				</a>
 			</div>
 		</div>
 		<nav id="site-navigation" class="small-12 large-8 columns" data-toggler>
