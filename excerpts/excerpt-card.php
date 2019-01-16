7914f82b7bb8e35927b0cff28fc27e5c433366bd
Leesa Ward <leesa@doubleedesign.com.au>
2019-01-17 00:09:10 +1100

    Tidied up excerpts and consolidated code

--- excerpts/excerpt-card.php
+++ excerpts/excerpt-card.php
@@ -10,7 +10,7 @@
 
 <div class="card-wrapper small-12 medium-6 large-4 columns">
 
-	<div <?php post_class('card'); ?>>
+	<div <?php post_class('excerpt card'); ?>>
 
 		<?php
 		$format = get_post_format();
@@ -18,26 +18,11 @@
 		$category = get_the_category();
 		?>
 
-		<div class="labels image nested row">
-			<div class="meta small-12 columns">
-				<div class="inner">
-					<?php
-					if($type == 'post') {
-						if ($format == 'video') {
-							echo '<span class="label shrink columns"><i class="fa fa-play-circle"></i></span>';
-						} else {
-							echo '<span class="label shrink columns"><i class="fa fa-newspaper-o"></i></span>';
-						}
-						echo '<span class="label shrink columns"><a href="/category/' . $category[0]->slug . '">' . $category[0]->name . '</a></span>';
-					}
-					?>
-				</div>
-			</div>
-		</div>
+		<?php include('meta.php'); // get_template_part doesn't work for same subfolder ?>
 
 		<div class="card-section">
 			<div class="wrap">
-				<?php the_title( '<h3>', '</h3>' ); ?>
+				<h2><?php the_title(); ?></h2>
 				<?php
 				// Use the manual excerpt if set
 				if(has_excerpt()) {
--- excerpts/excerpt-list.php
+++ excerpts/excerpt-list.php
@@ -28,52 +28,35 @@ if ($format == 'video') {
 }
 ?>
 
-<<?php echo $tag; ?> <?php post_class('excerpt-wrapper excerpt-list-style row collapse'); ?>>
+<<?php echo $tag; ?> <?php post_class('excerpt excerpt-list-style row collapse'); ?>>
 
 	<div class="nested row">
 
-			<?php if(has_post_thumbnail()) { ?>
-				<div class="image small-12 medium-4 columns">
-					<?php the_post_thumbnail('medium'); ?>
-				</div>
-				<div class="content small-12 medium-8 columns">
-			<?php } else { ?>
-				<div class="content small-12 columns">
-			<?php } ?>
+		<?php if(has_post_thumbnail()) { ?>
+			<div class="image small-12 medium-4 columns">
+				<?php the_post_thumbnail('medium'); ?>
+			</div>
+			<div class="content small-12 medium-8 columns">
+		<?php } else { ?>
+			<div class="content small-12 columns">
+		<?php } ?>
 
-			<?php
-			// Meta/labels for posts
-			if($type == 'post') { ?>
-				<div class="meta row">
-					<div class="small-12 columns">
-					<?php
-					if ($format == 'video') {
-						echo '<span class="secondary label"><i class="fas fa-play-circle"></i></span>';
-					} else {
-						echo '<span class="secondary label"><i class="fas fa-newspaper-o"></i></span>';
-					}
-					echo '<span class="secondary label"><a href="/category/'. $category[0]->slug .'">' . $category[0]->name . '</a></span>';
-					?>
-					</div>
-				</div>
-			<?php } ?>
+		<?php include('meta.php'); // get_template_part doesn't work for same subfolder ?>
 
-			<h2><?php the_title(); ?></h2>
+		<h2><?php the_title(); ?></h2>
 
-			<?php
-			// If manual excerpt is set, use that
-			if(has_excerpt()) {
-				the_excerpt();
-			} else {
-				// Otherwise, use custom excerpt function,
-				// which can be found in functions/developer.php
-				echo doublee_custom_excerpt(get_the_content(), 30);
-			} ?>
-			<a class="small button" href="<?php the_permalink(); ?>">
-				<?php echo $button_text; ?>
-			</a>
-
-		</div>
+		<?php
+		// If manual excerpt is set, use that
+		if(has_excerpt()) {
+			the_excerpt();
+		} else {
+			// Otherwise, use custom excerpt function,
+			// which can be found in functions/developer.php
+			echo doublee_custom_excerpt(get_the_content(), 30);
+		} ?>
+		<a class="small button" href="<?php the_permalink(); ?>">
+			<?php echo $button_text; ?>
+		</a>
 
 	</div>
 
