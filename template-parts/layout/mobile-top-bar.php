173fd5342a0ad81b744071d89bdd9088287174ec
Leesa Ward <leesa@doubleedesign.com.au>
2019-01-16 23:24:23 +1100

    Put layout template parts into template-parts folder

---
+++ template-parts/layout/mobile-top-bar.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Template part for mobile topbar menu
+ *
+ * @package WordPress
+ * @subpackage Double-E Foundation
+ * @since Double-E Foundation 3.0
+ */
+
+?>
+
+<div id="mobile-topbar-menu">
+	<nav class="vertical menu" data-accordion-menu role="navigation">
+	  <?php doublee_mobile_nav(); ?>
+	</nav>
+</div>
