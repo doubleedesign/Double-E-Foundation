0b4111de7fcbefd58f2e1b830fcfdc2f87010a1b
Leesa Ward <leesa@doubleedesign.com.au>
2019-01-17 00:48:37 +1100

    Improvements to how child pages can be displayed

--- template-parts/children.php
+++ template-parts/children.php
@@ -8,8 +8,8 @@
  */
 ?>
 
-<?php 
-// WP_Query arguments
+<?php
+$count = 0;
 $args = array( 
 	'posts_per_page'	=> -1,
 	'orderby'			=> 'menu_order',
@@ -17,25 +17,49 @@ $args = array(
 	'post_parent' 		=> $post->ID,
 	'post_type' 		=> 'page',
 	'post_status' 		=> 'publish', 
-); 
-
-// The Query
+);
 $childpages = new WP_Query( $args );
-
-// The Loop
 if ( $childpages->have_posts() ) { ?>
-	<div id="children" class="row">
+	<section id="children">
 		<?php while ( $childpages->have_posts() ) {
 			$childpages->the_post();
-			get_template_part('excerpts/excerpt-card');
-		} ?>
-	</div>
-
+			$count++;
+			?>
+			<?php
+			// Default order is image then content, stacked on mobile
+			// Adjust order through classes to alternate order on on larger screens
+			if ($count % 2 == 0) { // if $count is even
+				$even_odd = 'even';
+				$content_classes = 'small-12 large-6 columns large-order-1';
+				$image_classes = 'small-12 large-6 columns large-order-2';
+			} else { // $count is odd
+				$even_odd = 'odd';
+				$content_classes = 'small-12 large-6 columns';
+				$image_classes = 'small-12 large-6 columns';
+			} ?>
+			<div <?php post_class('child-page copy-image ' . $even_odd); ?>>
+				<div class="row align-center">
+					<?php if(has_post_thumbnail()) { ?>
+						<div class="image <?php echo $image_classes; ?>">
+							<?php the_post_thumbnail('large'); ?>
+						</div>
+					<?php } ?>
+					<div class="content <?php echo $content_classes; ?>">
+						<h2><?php the_title(); ?></h2>
+						<?php
+						// Use the manual excerpt if set
+						if(has_excerpt()) {
+							the_excerpt();
+							// Otherwise, use custom excerpt function,
+							// which can be found in functions/developer.php
+						} else {
+							echo doublee_custom_excerpt(get_the_content(), 30);
+						} ?>
+					</div>
+				</div>
+			</div>
+		<?php } ?>
+	</section>
 <?php
-} else {
-	// no posts found
 }
-
-// Restore original Post Data
 wp_reset_postdata();
-?>
