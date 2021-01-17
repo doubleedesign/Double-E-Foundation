557e0700f7830cdceddda52cfe2816fdfe6e62fb
Leesa Ward <leesa@doubleedesign.com.au>
2021-01-17 22:51:33 +1100

    Remove some more unused stuff

--- header.php
+++ header.php
@@ -16,7 +16,6 @@
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<meta http-equiv="X-UA-Compatible" content="IE=Edge" />
-		<?php get_template_part('template-parts/header/favicons'); ?>
 		<?php get_template_part('template-parts/header/preloader'); ?>
 		<?php wp_head(); ?>
 	</head>
