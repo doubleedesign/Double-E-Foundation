74bfa4d60c81ba4cfaa3448a771bf35402cdfebf
Leesa Ward <leesa@doubleedesign.com.au>
2021-01-28 18:35:06 +1100

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,15 @@
-const gulp 				= require('gulp');
-const plumber 			= require('gulp-plumber');
-const sass 				= require('gulp-sass');
-const sassGlob 			= require('gulp-sass-glob');
-const rename 			= require('gulp-rename');
-const autoprefixer 		= require('gulp-autoprefixer');
-const minifyCSS       	= require('gulp-minify-css');
-const uglify          	= require('gulp-uglify');
-const concat          	= require('gulp-concat');
-const gutil           	= require('gulp-util');
-const browsersync     	= require('browser-sync').create();
-const sourcemaps      	= require('gulp-sourcemaps');
+const gulp 			= require('gulp');
+const plumber 		= require('gulp-plumber');
+const sass 			= require('gulp-sass');
+const sassGlob 		= require('gulp-sass-glob');
+const rename 		= require('gulp-rename');
+const autoprefixer 	= require('gulp-autoprefixer');
+const minifyCSS     = require('gulp-minify-css');
+const uglify        = require('gulp-uglify');
+const concat        = require('gulp-concat');
+const gutil         = require('gulp-util');
+const browsersync   = require('browser-sync').create();
+const sourcemaps	= require('gulp-sourcemaps');
 
 gulp.task('styles',  done =>  {
 	gulp.src('scss/style.scss')
